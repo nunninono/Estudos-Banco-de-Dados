5e6b2eeae456fef3d03e64d579f3cfbf4eda4e8c
Nunno Wakiyama Diniz Carvalho <142551010+nunninono@users.noreply.github.com>
2025-04-17 16:53:55 -0300

    Update DATABASE 01.sql

--- Estudos/DATABASE 01.sql
+++ Estudos/DATABASE 01.sql
@@ -1,11 +1,12 @@
 CREATE DATABASE IF NOT EXISTS Estudos; 
 USE Estudos;
 CREATE TABLE IF NOT EXISTS Estudantes (
-	ID INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(30) NOT NULL,
+    ID INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(30) NOT NULL,
     cpf VARCHAR(14) NOT NULL,
     curso VARCHAR(20) NOT NULL,
-    periodo SMALLINT);
+    periodo SMALLINT
+);
     
 INSERT INTO Estudantes (nome, cpf, curso, periodo) VALUES ('Claudio', '111.222.333-44', 'COMPUTACAO', 3);
 SELECT * FROM Estudantes;
