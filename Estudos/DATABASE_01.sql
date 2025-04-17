94b51de61217205a7b1a5c107e00fc9597245a9b
Nunno Wakiyama Diniz Carvalho <142551010+nunninono@users.noreply.github.com>
2025-04-17 18:09:13 -0300

    Create DATABASE_01.sql

---
+++ Estudos/DATABASE_01.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS Estudos; 
+USE Estudos;
+CREATE TABLE IF NOT EXISTS Estudantes (
+	ID INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(30) NOT NULL,
+    cpf VARCHAR(14) NOT NULL,
+    curso VARCHAR(20) NOT NULL,
+    periodo SMALLINT);
+    
+INSERT INTO Estudantes (nome, cpf, curso, periodo) VALUES ('Claudio', '111.222.333-44', 'COMPUTACAO', 3);
+SELECT * FROM Estudantes;
+
+##DROP TABLE Estudantes;
